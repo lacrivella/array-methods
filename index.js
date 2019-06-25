f3c73ad2092da15132a941d69d8e901745db47c2
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-25 11:40:44 -0700

    reduceFunction walkthrough with Ryan

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ function findIndex(arr, callback) {
   return -1;
 }
 
-/*
-function reduceFunction(arr, callback) {
-  const reduced = [];
+function reduceFunction(arr, callback, initalValue) {
+  let acc = initalValue;
   for(let i = 0; i < arr.length; i++) {
-
+    acc = callback(acc, arr[i]);
   }
-  return reduced;
+  return acc;
 }
 
-*/
+
 module.exports = {
   mapFunction,
   filterFunction,
-  findIndex
+  findIndex,
+  reduceFunction
 };
--- index.test.js
+++ index.test.js
@@ -1,17 +1,23 @@
-const { mapFunction, filterFunction, findIndex } = require('./index');
+const { 
+  mapFunction, 
+  filterFunction, 
+  findIndex,
+  reduceFunction
+} = require('./index');
 
+//mapFunction
 it('doubles array', () => {
   const arr = [1, 2, 3, 4];
   const double = mapFunction(arr, arr => arr * 2);
   expect(double).toEqual([2, 4, 6, 8]);
 });
-
+//filterFunction
 it('filters array', () => {
   const numbers = [1, 2, 3, 4];
   const onlyEvens = filterFunction(numbers, number => number % 2 === 0);
   expect(onlyEvens).toEqual([2, 4]);
 });
-
+//findIndex
 it('findIndex returns a number', () => {
   const colors = ['red', 'blue', 'green'];
   const index = findIndex(colors, color => color.includes('l'));
@@ -29,3 +35,22 @@ it('returns index', () => {
   const index = findIndex(colors, color => color.includes('s'));
   expect(index).toEqual(-1);
 });
+
+//reduceFunction
+it('iterates through array and invokes the callback', () => {
+  const numbers = [1, 2, 3];
+  const callback = jest.fn();
+
+  reduceFunction(numbers, callback);
+
+  expect(callback).toHaveBeenCalledTimes(numbers.length);
+});
+
+it('iterates through array and sets acc', () => {
+  const numbers = [1, 2, 3];
+  const sum = reduceFunction(numbers, (acc, item) => acc + item, 0);
+
+  expect(sum).toEqual(6);
+});
+
+//
